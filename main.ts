f1046f29cc6496f86d91a806b617e18b7d99f5b8
ElSantiPa08 <108404412+ElSantiPa08@users.noreply.github.com>
2022-06-29 08:22:38 -0300

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    if (true) {
+        lista = game.createSprite(1, 1)
+        game.setLife(3)
+    }
+})
+let lista: game.LedSprite = null
+basic.showString("Hola")
+basic.pause(100)
+basic.showString("Pulse \"A\" para iniciar")
