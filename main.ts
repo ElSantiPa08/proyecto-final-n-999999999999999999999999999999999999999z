f6b61b884068d3c55b39485fa10e276c09a3ab9a
ElSantiPa08 <108404412+ElSantiPa08@users.noreply.github.com>
2022-06-29 08:43:32 -0300

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,21 @@
 input.onButtonPressed(Button.A, function () {
+    sprite.turn(Direction.Left, 45)
+})
+input.onButtonPressed(Button.AB, function () {
     if (true) {
-        lista = game.createSprite(1, 1)
+        lista = game.createSprite(1, 2)
         game.setLife(3)
     }
 })
+input.onButtonPressed(Button.B, function () {
+    sprite.move(1)
+})
 let lista: game.LedSprite = null
+let sprite: game.LedSprite = null
 basic.showString("Hola")
 basic.pause(100)
-basic.showString("Pulse \"A\" para iniciar")
+basic.showString("Pulse \"A\" girar 45° a la izquierda")
+basic.pause(100)
+basic.showString("Pulse \"B\" para avanzar")
+basic.pause(100)
+basic.showString("Pulse \"A\" y \"B\" para iniciar ")
